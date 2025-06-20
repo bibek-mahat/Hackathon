9a30dc6c0af65a4c634b84bf7adfe61df0e5d88d
prajal-shrestha <shreprajal@gmail.com>
2025-06-20 11:26:15 +0545

    chore

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,9 +1,11 @@
 import Card from "./Card";
 import "./Body.css";
+import CitizenEngagementBanner from "./CitizenEngagementBanner";
 const Body = () => {
   return (
     <>
       <div className="container">
+        <CitizenEngagementBanner />
         <Card />
       </div>
     </>
